6eb0f9e19dfd61a1a75ffe47d5844a3519368d9c
Enrique Pedruelo <eiglesias@juniper.net>
2020-06-16 16:57:25 +0200

    Moved file related methods in Decryptor class to util/files.py, adapt remaining methods to it and add apply_decryption()

--- vcsd/decryptor.py
+++ vcsd/decryptor.py
@@ -4,107 +4,18 @@ import qrcode
 import numpy as np
 from PIL import Image
 
+import vcsd.util as util
+
 # TODO docstring for CvedDecryptor class
-class CvedDecryptor():
-    emojis = {
-        "check_mark_button":    "\U00002705",
-        "cross_mark":           "\U0000274C",
-        "magnifying_glass":     "\U0001F50D"
-    }
+class Decryptor():
+    """
+    """
 
-    # qr_code_size: qr_code_version
-    qr_size_version_dict = {
-        21:  1,  	 25:  2,  	 29:  3,  	 33:  4,  	 37:  5,  	
-        41:  6,  	 45:  7,  	 49:  8,  	 53:  9,  	 57:  10,  	
-        61:  11,  	 65:  12,  	 69:  13,  	 73:  14,  	 77:  15,  	
-        81:  16,  	 85:  17,  	 89:  18,  	 93:  19,  	 97:  20,  	
-        101: 21,  	 105: 22,  	 109: 23,  	 113: 24,  	 117: 25,  	
-        121: 26,  	 125: 27,  	 129: 28,  	 133: 29,  	 137: 30,  	
-        141: 31,  	 145: 32,  	 149: 33,  	 153: 34,  	 157: 35,  	
-        161: 36,  	 165: 37,  	 169: 38,  	 173: 39,  	 177: 40
-    }
 
-    def save_im(self, im, path_im, im_name="/image_gen.png"):
-        """Save an image file from an image object
+    def apply_decryption(self, path_im_A="", path_im_B="", save_im_extracted_QR=False, path_im_extracted_QR=""):
+        """Obtains the data hidden in two transparencies
         
-        Save a file with an image generated from an incoming image object
-
-        Parameters
-        ----------
-        im: object
-            Image object, instance of the PIL Image class
-        path_im: str
-            Absolute path where to store the qr image file
-        im_name: str, default="/image_gen.png"
-            Name for the generated file
-        """
-        if(path_im==""):
-            try:
-                dirpath = os.getcwd() + im_name
-                im.save(dirpath, 'png')
-                print(self.emojis["check_mark_button"] + f" Image successfully saved at {dirpath}")
-            except:
-                print(self.emojis["cross_mark"] + " Unable to correctly save the image")
-        else:
-            try:
-                im.save(path_im, 'png')
-                print(self.emojis["check_mark_button"] + f" Image successfully saved at {path_im}")
-            except:
-                print(self.emojis["cross_mark"] + " Unable to find the path specified")
-
-
-    def load_im(self, path_im=""):
-        """Load an image file 
-
-        Load an image file (in .png format) and return it as a PIL Image object
-
-        Parameters
-        ----------
-        path_im: str
-            Absolute path where the image (in .png format) is stored
-
-        Returns
-        -------
-        im: object
-            Image object, instance of the PIL Image class
-        """
-        if(path_im!=""):
-            try:
-                im = Image.open(path_im, mode="r")
-                print(self.emojis["check_mark_button"] + f" Image successfully loaded from {path_im}")
-                return im
-            except:
-                print(self.emojis["cross_mark"] + f" Unable to correctly load the image from {path_im}")
-        else:
-            print(self.emojis["cross_mark"] + " The path to the image was not provided")
-
-
-    def gen_transparence_from_image(self, im):
-        """Generate transparence 2D array from an image
-        
-        Generate a transparence (2D numpy array) from an image object (ready to be saved or plotted) 
-
-        Parameters
-        ----------
-        im: object
-            Image object, instance of the PIL Image class
-        
-        Returns
-        -------
-        trans: ndarray
-            Transparence, a 2D array computed from the QR code
-        """
-        trans_rgb = np.array(im)
-        trans = trans_rgb.mean(axis=2).astype(int)
-        trans = (trans/255).astype(int)
-        return trans
-
-
-    def load_and_validate_trans(self, path_im_A="", path_im_B=""):
-        """Load and validate the image files of the pair of transparencies
-
-        Load the image files (in .png format) of the pair of transparencies, check if they are equal sized and the version of the QR code they contain.
-        If checks are successful return each transpacencie of the pair as a 2D numpy array.
+        Perform all the steps needed to retrieve the data encrypted in two transparencies by visual-crypt
 
         Parameters
         ----------
@@ -112,32 +23,26 @@ class CvedDecryptor():
             Absolute path where the image (in .png format) of the transparence A is stored
         path_im_B: str
             Absolute path where the image (in .png format) of the transparence B is stored
+        save_im_extracted_QR: bool, default=False
+            Whether to save recovered QR or not (location by default is current directory)
+        path_im_extracted_QR: str
+            Absolute path where to store the image file of the qr code extracted
 
         Returns
         -------
-        trans_A: ndarray
-            Transparence A, a 2D array computed from the QR code
-        trans_B: ndarray
-            Transparence B, a 2D array computed from the QR code
+        data: str
+            Text data extracted from the QR code
         """
-        # load images with transparencies
-        im_trans_A = self.load_im(path_im=path_im_A)
-        im_trans_B = self.load_im(path_im=path_im_B) 
-        
-        # extract binary matrices
-        trans_A = self.gen_transparence_from_image(im_trans_A)
-        trans_B = self.gen_transparence_from_image(im_trans_B)
-        
-        # determine version of the hidden qr code
-        if(len(trans_A) == len(trans_B)):
-            print(emojis["check_mark_button"] + " Loaded transparences are equally sized")
-            qr_size = round(len(trans_A)/2)
-            detected_version = qr_size_version_dict[qr_size]
-            print(emojis["magnifying_glass"] + f" Loaded transparences correspond to a QR code version {detected_version}")
-        else:
-            print(emojis["cross_mark"] + f" There is size mismatch between both transparencies: {trans_A.shape} and {trans_B.shape}. They need to be equally sized")
-            
-        return trans_A, trans_B
+        # 1 - Load transparencies from the images
+        trans_A, trans_B = util.load_and_validate_trans(path_im_A=path_im_A, path_im_B=path_im_B)
+        # 2 - Extract the QR code encoded in the transparencies
+        qr_matrix_rec = self.extract_qr_from_transparences(trans_A, trans_B)
+        # 3 - Extract the data encoded in the QR code
+        data, im_qr_extracted  = self.extract_data_from_qr_matrix(qr_matrix_rec)
+        if(save_im_extracted_QR):
+            util.save_im(im=im_qr_extracted, path_im=path_im_extracted_QR, im_name="/QR_rec.png")
+
+        return data
 
 
     # TODO create extract_LSB
@@ -176,7 +81,7 @@ class CvedDecryptor():
         return qr_matrix_rec
 
 
-    def extract_data_from_qr_matrix(self, qr_matrix_rec, save_im=False, path_im_QR_rec=""):
+    def extract_data_from_qr_matrix(self, qr_matrix_rec):
         """Extract the data from the QR code specified
         
         Extract the text data from the QR code matrix specified
@@ -185,32 +90,26 @@ class CvedDecryptor():
         ----------
         qr_matrix_rec: ndarray
             QR code matrix, a 2D array representing the QR code
-        save_im: bool, default=False
-            Whether to save QR or not (location by default ir current directory)
-        path_im_QR_rec: str
-            Absolute path where to store the image file of the qr code introduced
 
         Returns
         -------
         data: str
             The text data extracted from the QR code
+        im_qr_extracted: object
+            Image object with the recovered QR code, instance of the PIL Image class
         """
         # convert that matrix into qr_code object
-        qr_version = qr_size_version_dict[len(qr_matrix_rec)]
+        qr_version = util.QR_SIZE_VERSION_DICT[len(qr_matrix_rec)]
         qr_code = qrcode.QRCode(version=qr_version, border=1)
         qr_code.make(fit=True)
         qr_code.modules = qr_matrix_rec
         
         # obtain image object with the qr code extracted
         im_qr_extracted = qr_code.make_image()
-
-        # this should be separated in other function (maybe the composed ones)
-        if(save_im):
-            self.save_im(im=im_qr_extracted, path_im=path_im_QR_rec)
         
         # use cv2 to extract the data from the qr image object
-        im_qr_extracted = np.asarray(im_qr_extracted.convert('RGB'))
+        im_qr_extracted_arr = np.asarray(im_qr_extracted.convert('RGB'))
         detector = cv2.QRCodeDetector()
-        data, bbox, straight_qrcode = detector.detectAndDecode(im_qr_extracted)
+        data, bbox, straight_qrcode = detector.detectAndDecode(im_qr_extracted_arr)
         
-        return data
+        return data, im_qr_extracted
